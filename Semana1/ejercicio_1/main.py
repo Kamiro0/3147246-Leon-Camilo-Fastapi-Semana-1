e1a1fd35d6d4ce74be120e9f32da2a2226e319b1
¨Camilo <camiloleon981@gmail.com>
2025-08-07 23:47:47 -0500

    mi primera api fastapi

---
+++ Semana1/ejercicio_1/main.py
@@ -0,0 +1,26 @@
+# Actualizar main.py (agregar solo al final)
+from fastapi import FastAPI
+
+app = FastAPI(title="Mi Primera API")
+
+@app.get("/")
+def hello_world():
+    return {"message": "¡Mi primera API FastAPI!"}
+
+@app.get("/info")
+def info():
+    return {"api": "FastAPI", "week": 1, "status": "running"}
+
+@app.get("/greeting/{name}")
+def greet_user(name: str):
+    return {"greeting": f"¡Hola {name}!"}
+
+@app.get("/my-profile")
+def my_profile():
+    return {
+        "name": "Camilo Leon",           
+        "bootcamp": "FastAPI",
+        "week": 1,
+        "date": "2025",
+        "likes_fastapi": True            
+    }
